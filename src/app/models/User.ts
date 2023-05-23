a5c0fc54a5a717ac0c85ddaf5c802eebf28c4a44
Jose Alcayaga <jose64am@aol.com>
2023-05-23 00:06:27 -0400

    AN-128 feat: added basic register page submit logic

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,12 @@
 export default interface User {
-    name: string;
-    firstLastName: string;
-    firstSecondName: string;
-    id: string;
-    email: string;
-    gender: string;
-    phone: number;
-    password: string;
+    name?: string;
+    firstLastName?: string;
+    firstSecondName?: string;
+    rut?: string;
+    email?: string;
+    gender?: string;
+    phone?: number;
+    isEnabled: boolean;
+    password?: string;
+    type: number;
 }
