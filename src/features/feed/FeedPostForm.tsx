0af9349a9f1f8fbd87a98e5b07b5ef7b761d9dda
Jose Alcayaga <jose64am@aol.com>
2023-05-31 00:40:31 -0400

    AN-153 fix: converted modal to normal element, added missing validation

--- src/features/feed/FeedPostForm.tsx
+++ src/features/feed/FeedPostForm.tsx
@@ -10,7 +10,7 @@ import {
     Stack,
     Typography,
     Button,
-    DialogActions
+    DialogActions, CardActions, CardHeader, CardContent
 } from '@mui/material';
 import TitleIcon from '@mui/icons-material/Title';
 import ArticleIcon from '@mui/icons-material/Article';
@@ -21,20 +21,38 @@ import { purple } from '@mui/material/colors';
 import { Controller, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import {useNavigate} from "react-router-dom";
 import agent from '../../app/api/agent';
+import Grid from "@mui/material/Grid";
 
 export default function FeedPostForm()
 {
     const { control, handleSubmit, formState: { isSubmitting, errors, isValid } } = useForm({mode: 'onTouched'});
+    const [openConfirmation, setOpenConfirmation] = React.useState(false);
 
     return (
         <>
-            <Dialog open={true} fullWidth sx={{m: 2}}>
-                <DialogTitle>
-                    <Card sx={{bgcolor: 'gray'}}>
+        <Dialog open={openConfirmation}
+                onClose={()=>{setOpenConfirmation(false)}}>
+            <DialogTitle id="alert-dialog-title">
+                {"¿Está seguro que quiere agregar la noticia?"}
+            </DialogTitle>
+            <DialogActions>
+                <Button onClick={()=>{setOpenConfirmation(false)}}>Cancelar</Button>
+                <Button onClick={()=>{setOpenConfirmation(false)}} autoFocus>
+                    Aceptar
+                </Button>
+            </DialogActions>
+        </Dialog>
+        <Grid container component="main"
+              spacing={0}
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{ minHeight: '100vh' }}>
+            <Card sx={{width: '75%'}}>
+                <CardContent>
+                    <Card sx={{color: 'white', bgcolor: 'gray', my: 2}}>
                         <Typography align="center" sx={{my: 2, fontWeight: 'bold' }} variant="h4">Nueva Noticia</Typography>
                     </Card>
-                </DialogTitle>
-                <DialogContent>
                     <Stack>
                         <Box>
                             <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
@@ -93,11 +111,12 @@ export default function FeedPostForm()
                             </Box>
                         </Box>
                     </Stack>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant="contained"> <AddToPhotosIcon sx={{ mr: 1, my: 0.5 }} /> Agregar Noticia</Button>
-                </DialogActions>
-            </Dialog>
+                </CardContent>
+                <CardActions sx={{ flexDirection: 'row-reverse', m: '2' }}>
+                    <Button variant="contained" onClick={()=>{setOpenConfirmation(true)}}> <AddToPhotosIcon sx={{ mr: 1, my: 0.5 }} /> Agregar Noticia</Button>
+                </CardActions>
+            </Card>
+        </Grid>
         </>
     )
 }
