def8e835aa493e2abbeb0b9548e0eddc9268f23b
Jose Alcayaga <jose64am@aol.com>
2023-06-06 03:05:09 -0400

    AN-292 feat: added account slice

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { accountSlice } from "../../features/account/accountSlice";
 
 export const store = configureStore({
-    reducer: {},
-})
+    reducer: {
+        account: accountSlice.reducer
+    },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
