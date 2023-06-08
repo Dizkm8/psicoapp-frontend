782d9232080b66cdc301539b58971d19f68939ca
Jose Alcayaga <jose64am@aol.com>
2023-06-07 22:19:22 -0400

    AN-301 feat: reworked update profile page

--- src/features/account/Profile.tsx
+++ src/features/account/Profile.tsx
@@ -2,12 +2,14 @@ import Typography from "@mui/material/Typography";
 import * as React from "react";
 import ChangePassword from "./ChangePassword";
 import PermanentDrawerLeft from "../../app/components/Layout";
+import UpdateProfile from "./UpdateProfile";
 
 export default function Profile()
 {
     return (
         <>
             <PermanentDrawerLeft />
+            <UpdateProfile/>
             <ChangePassword/>
         </>
     )
