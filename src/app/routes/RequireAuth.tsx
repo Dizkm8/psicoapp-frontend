4a78447a960a7a40a8d5a2a3be8aa265f80557d4
Jose Alcayaga <jose64am@aol.com>
2023-06-08 04:23:10 -0400

    AN-338 feat: added user name to navbar account menu

--- src/app/routes/RequireAuth.tsx
+++ src/app/routes/RequireAuth.tsx
@@ -1,10 +1,9 @@
 import {Outlet, Navigate} from 'react-router-dom'
 import {useSelector} from "react-redux";
-import {selectRole} from "../../features/account/accountSlice";
+import {selectName, selectRole} from "../../features/account/accountSlice";
 interface Props {
     role?: number;
 }
-
 export default function RequireAuth({role}: Props)
 {
     const userRole: number | null = useSelector(selectRole);
